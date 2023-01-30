1bffa66c53edb35786eacf768eb3b7e38ecfc44b
Stéphane Cavillon <stephane.cavillon@itech.fr>
2023-01-30 17:23:48 +0100

    🚨 lint stuff

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -19,7 +19,7 @@ const mapDispatchToProps = (dispatch) => ({
 
 const ItemPreview = (props) => {
   const item = props.item;
-  const defaultImg = "/placeholder.png"
+  const defaultImg = "/placeholder.png";
   const handleClick = (ev) => {
     ev.preventDefault();
     if (item.favorited) {
