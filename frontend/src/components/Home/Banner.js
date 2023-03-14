338348268dcf7c0a2bab3cd7326e596d67b4951c
Stéphane Cavillon <stephane.cavillon@itech.fr>
2023-03-14 15:09:16 +0100

    add search on the front end

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,7 +1,17 @@
 import React from "react";
 import logo from "../../imgs/logo.png";
+import agent from '../../agent'
 
-const Banner = () => {
+const Banner = (props) => {
+  const onSearchChange = (event) => {
+    console.log('onchange', props)
+    props.onSearchFilter(
+      event.target.value,
+      (page) =>
+        agent.Items.byTitle(event.target.value),
+        agent.Items.byTitle(event.target.value)
+      )
+    };
   return (
     <div className="banner text-white">
       <div className="container p-4 text-center">
@@ -9,6 +19,15 @@ const Banner = () => {
         <div>
           <span>A place to </span>
           <span id="get-part">get</span>
+          <form>
+            <input
+              type="text"
+              placeholder='what you want'
+              id='search'
+              name='search'
+              onChange={onSearchChange}
+            ></input>
+          </form>
           <span> the cool stuff.</span>
         </div>
       </div>
